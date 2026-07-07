8972c285269f3a67f59ecb72a997a33261ccf626
Oliver Maglana <jamols09@gmail.com>
2026-07-07 18:25:02 +0800

    feat: override tasks view on admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Task
 
+
+# Override Task on admin side
+# This will show the columns on the admin side
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'updated_at')
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
