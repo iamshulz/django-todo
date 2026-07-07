fdc19c431fe2823d182dc43402cce579203c8a82
Oliver Maglana <jamols09@gmail.com>
2026-07-07 19:04:08 +0800

    feat: get post data and save

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def addTask(request):
-    return HttpResponse('The form is submitted')
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
