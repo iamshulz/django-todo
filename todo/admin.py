fa069f2b6b88e180ab4caf8b03424a4b01dbedb6
Oliver Maglana <jamols09@gmail.com>
2026-07-07 18:27:42 +0800

    feat: enable search field for task name

--- todo/admin.py
+++ todo/admin.py
@@ -4,8 +4,10 @@ from .models import Task
 
 # Override Task on admin side
 # This will show the columns on the admin side
+# See More here: https://docs.djangoproject.com/en/6.0/ref/contrib/admin/
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task')
 
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
