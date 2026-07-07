a770cbe06536a2800db0cc470f830bd6be6e67c1
Oliver Maglana <jamols09@gmail.com>
2026-07-07 18:46:54 +0800

    feat: add task functionality

--- todo/admin.py
+++ todo/admin.py
@@ -6,8 +6,8 @@ from .models import Task
 # This will show the columns on the admin side
 # See More here: https://docs.djangoproject.com/en/6.0/ref/contrib/admin/
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('task', 'is_completed', 'updated_at')
-    search_fields = ('task')
+    list_display = ('task', 'is_completed', 'updated_at',) #add comma to the last item for good pratice
+    search_fields = ('task',) #add comma to the last item for good pratice
 
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
