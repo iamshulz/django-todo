b4dd12ec2e47698284e8fc3d4f2ab4e9a7a86a5a
Oliver Maglana <jamols09@gmail.com>
2026-07-07 20:17:31 +0800

    feat: add edit delete func

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,8 @@ from .import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name = 'addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('deleteTask/<int:pk>/', views.deleteTask, name='deleteTask'),
+    path('edit_task/<int:pk>/', views.editTask, name='editTask')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,31 @@
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
+from django.http import HttpResponse
 
 # Create your views here.
 def addTask(request):
     task = request.POST['task']
-    Task.objects.create(task=task)
+    Task.objects.create(task = task)
     return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def deleteTask(request, pk):
+    task = Task.objects.get(pk = pk)
+    task.delete()
+    return redirect('home')
+
+def editTask(request, pk):
+    
+    return render(request, 'edit_task.html')
